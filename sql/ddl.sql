04acfc90b765230ac9a9caf3ac1a6c740c61495d
genzouw <genzouw@gmail.com>
2013-05-30 21:57:55 +0900

    .htaccessによるアクセス制限処理を追加

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -16,14 +16,26 @@ grant
 flush privileges;
 
 
-CREATE TABLE position (
+create table positions (
+    id int not null auto_incremennt primary key,
+    group_id int,
     ip_address varchar(15) not null,
     lat text,
     lng text,
     screen_name text,
-    update_date timestamp,
-    primary key (ip_address)
+    created datetime default null,
+    modified datetime default null
 );
+
+create unique index uq_position_id_address
+    on position (id_address)
+;
+
+create index idx_position_group_id
+    on position (group_id)
+;
 
 alter table position add column screen_name text;
 alter table position add column update_date timestamp;
+
+
